387423180002f1d38122738f1e0e79cf42bce071
ZhipengLI <lizhipeng524@gmail.com>
2016-10-11 17:22:32 +0800

    Add files via upload

---
+++ ConnectionPool/ConnectionDriver.java
@@ -0,0 +1,5 @@
+package ConnectionPool;
+
+public class ConnectionDriver {
+
+}
---
+++ ConnectionPool/ConnectionPool.java
@@ -0,0 +1,7 @@
+package ConnectionPool;
+import java.sql.Connection;
+import java.util.*;
+public class ConnectionPool {
+	private LinkedList<Connection> pool=new LinkedList<Connection>();
+	
+}
